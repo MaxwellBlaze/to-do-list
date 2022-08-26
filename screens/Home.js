d2fc04a24cb5afd711e2b351ec1acf1195d732a9
Heinh <heinh97@gmail.com>
2022-08-26 21:38:35 +0800

    add list function works

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, } from 'react';
-import {StyleSheet, View, Text, Image, Button, FlatList, TouchableOpacity, TextInput, Keyboard} from 'react-native';
+import {StyleSheet, View, Text, Image, Button, FlatList, TouchableOpacity, TextInput, Keyboard, Modal, Alert} from 'react-native';
 import { firebase } from '../firebase/config';
 import { useNavigation } from '@react-navigation/native';
 import { FontAwesome } from '@expo/vector-icons';
@@ -7,28 +7,46 @@ import { FontAwesome } from '@expo/vector-icons';
 const Home = ({navigation}) => {
 
     //for managing lists, do the same for tasks in ListView screen
-    const [lists, setLists] = useState(['Shopping', 'To Do', 'Chores']);
+    const [lists, setLists] = useState([]);
     const listsRef = firebase.firestore().collection('lists');
     const [addList, setAddList] = useState('');
 
+    //for + List modal
+    const [modalVisible, setModalVisible] = useState(false);
+
     //get data from firebase
-    // useEffect(() => {
-    //     listsRef
-    //     .orderBy('dateCreated', 'desc')
-    //     .onSnapshot(
-    //         querySnapshot => {
-    //             const lists = []
-    //             querySnapshot.firebase.forEach((doc) => {
-    //                 const {heading} = doc.data()
-    //                 lists.push({
-    //                     id: doc.id,
-    //                     heading,
-    //                 })
-    //             })
-    //             setLists(lists);
-    //         }
-    //         )
-    // }, []);
+    useEffect(() => {
+        // listsRef
+        // .orderBy('dateCreated', 'desc')
+        // .onSnapshot(
+        //     querySnapshot => {
+        //         const lists = []
+        //         querySnapshot.firebase.forEach((doc) => {
+        //             const {heading} = doc.data()
+        //             lists.push({
+        //                 id: doc.id,
+        //                 heading,
+        //             })
+        //         })
+        //         setLists(lists);
+        //     }
+        // )
+        listsRef.orderBy('dateCreated', 'desc').onSnapshot({
+            error: (e) => console.log(e),
+            next: (querySnapshot) => {
+                const lists = [];
+                querySnapshot.forEach((doc) => {
+                    const {heading} = doc.data()
+                    lists.push({
+                        id: doc.id,
+                        heading,
+                    })
+                })
+                setLists(lists);
+            }
+        })
+
+    }, []);
 
     //delete a list
     const deleteList = (lists) => {
@@ -60,7 +78,8 @@ const Home = ({navigation}) => {
             .catch((error) => {
                 alert(error);
             })
-        }
+        };
+        setModalVisible(!modalVisible);
     }
 
     return(
@@ -74,11 +93,17 @@ const Home = ({navigation}) => {
             
             {/* Flatlist of tasklists */}
             <View style={styles.bodyContainer}>
-                {/* <Text style={{textAlign: 'center', paddingTop: 5, fontSize: 18,}}> Task List 1 </Text>
-                <Text style={{textAlign: 'center', paddingTop: 5, fontSize: 18,}}> Task List 2 </Text>
-                <Text style={{textAlign: 'center', paddingTop: 5, fontSize: 18,}}> Task List 3 </Text> */}
+
                 <FlatList 
-                    
+                    data={lists}
+                    numColumns={1}
+                    renderItem={({item}) => (
+                        <View>
+                            <TouchableOpacity style={styles.list}>
+                                <Text style={{textAlign: 'center', paddingTop: 5, fontSize: 18,}}>{item.heading.toUpperCase()}</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )}
                 />
 
             </View>
@@ -94,7 +119,10 @@ const Home = ({navigation}) => {
 
             <View style={styles.buttonsContainer}>
                 {/* To do: onPress function--> bring user to list creation page */}
-                <TouchableOpacity style={[styles.button, {borderTopLeftRadius: 15, borderBottomLeftRadius: 15,}]}>
+                <TouchableOpacity 
+                    style={[styles.button, {borderTopLeftRadius: 15, borderBottomLeftRadius: 15,}]}
+                    onPress={() => setModalVisible(true)}
+                >
                     <Text style={styles.buttonText}>
                         + List
                     </Text>
@@ -115,13 +143,53 @@ const Home = ({navigation}) => {
                 </TouchableOpacity>
             </View>
     
-            
+            {/* add list modal */}
+            <View style={styles.centeredView}>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                    Alert.alert("Modal has been closed.");
+                    setModalVisible(!modalVisible);
+                    }}
+                >
 
-            <Button
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+
+                            <Text style={styles.headerText}>Add A List</Text>
+                            <TextInput 
+                                style={styles.textInput}
+                                placeholder='List Name'
+                                placeholderTextColor={'black'}
+                                onChangeText={(heading) => setAddList(heading)}
+                                value={addList}
+                            />
+
+                            <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
+                                <View style={[styles.button, {marginTop: 30, backgroundColor: 'maroon', }]}>
+                                    <Text style={[styles.buttonText, {fontSize: 16,}]}>Cancel</Text>
+                                </View>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity onPress={() => addNewList()}>
+                                <View style={[styles.button, {marginTop: 30,}]}>
+                                    <Text style={[styles.buttonText, {fontSize: 16,}]}>Create</Text>
+                                </View>
+                            </TouchableOpacity> 
+                            
+                        </View>
+                    </View>
+                </Modal>
+            </View>
+
+            {/* <Button
                 title="Go to Details"
                 onPress={() => navigation.navigate('Details')}
-            />
-        </View>
+            /> */}
+
+            </View>
     )
 }
 
@@ -180,6 +248,46 @@ const styles = StyleSheet.create({
         fontWeight: '600',
         color: 'white',
     },
+    centeredView: {
+        // flex: 1,
+        
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: '50%',
+        
+        
+      },
+      modalView: {
+        // flexDirection: 'column',
+        // height: 400,
+        // width: 250,
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+      },
+      textInput: {
+        // marginHorizontal: 30,
+        marginTop: 20,
+        padding: 15,
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: '#706a5d',
+      },
+      list: {
+        borderWidth: 0.3, 
+        margin:10,
+        padding: 5,
+        borderRadius: 10,
+        backgroundColor: 'white',
+
+      },
 
 
 });
