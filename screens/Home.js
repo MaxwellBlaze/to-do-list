e97cfa8fc0ecbaebc96c52d6b014d94367e2353e
Heinh <heinh97@gmail.com>
2022-08-28 02:49:02 +0800

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -526,7 +526,7 @@ const styles = StyleSheet.create({
         
         justifyContent: "center",
         alignItems: "center",
-        marginTop: '30%',
+        marginTop: '50%',
         
         
       },
